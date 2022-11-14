1ccbfa6e3cd5db6010edf40ba6953f5524e935b7
Nurdin <@n.ulanuulu>
2022-11-14 13:08:13 +0600

    task23

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import AboutUs from './components/about-us/AboutUs.js';
+import Portfolio from './components/portfolio/Portfolio.js';
 
 function App() {
     return (
         <div>
+            <AboutUs/>
+            <Portfolio/>
         </div>
     );
 }
---
+++ src/components/portfolio/Portfolio.js
@@ -0,0 +1,11 @@
+function Portfolio() {
+    return (
+        <div>
+            <h1>
+                Что-то обо мне
+            </h1>
+        </div>
+    );
+}
+
+export default Portfolio;
