c9c722ed3a035f28ac9bad03831710f15398e396
Nurdin <@n.ulanuulu>
2022-11-16 12:35:27 +0600

    task24

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import AboutUs from './components/about-us/AboutUs.js';
 import Portfolio from './components/portfolio/Portfolio.js';
 
 function App() {
+    const text = { title: 'hello', subtitle: 'lorem lorem lorem' };
+    
     return (
         <div>
-            <AboutUs/>
+            <AboutUs text={ text }/>
             <Portfolio/>
         </div>
     );
--- src/components/about-us/AboutUs.js
+++ src/components/about-us/AboutUs.js
@@ -1,6 +1,8 @@
-function AboutUs() {
+function AboutUs(props) {
     return (
         <div>
+            <h1>{ props.text.title }</h1>
+            <h1>{ props.text.subtitle }</h1>
             <h1>Улан уулу Нурдин</h1>
         </div>
     );
